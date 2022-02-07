f2f628f52149299df80d2aac24a5430a6d4785e1
Marta Nara <marta.kos1230@gmail.com>
2022-02-07 12:42:34 +0100

    Add SingleTable form in SingleTable.js, add API call for editing table data

--- src/components/pages/SingleTable.js
+++ src/components/pages/SingleTable.js
@@ -1,16 +1,76 @@
+import { Form, Button } from 'react-bootstrap';
 import { useParams } from 'react-router';
-import { useSelector } from 'react-redux';
-import { getTableId } from '../../redux/tablesRedux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTableId, updateSingleTable } from '../../redux/tablesRedux';
+import { useState } from 'react';
+import shortid from 'shortid';
+import { useNavigate } from 'react-router-dom';
 
 const SingleTable = () => {
 
   const { id } = useParams();
   const table = useSelector(state => getTableId(state, parseInt(id)))
-  console.log(table);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const [status, setStatus] = useState(table.status)
+  const [peopleAmount, setPeopleAmount] = useState(table.peopleAmount)
+  const [maxPeopleAmount, setMaxPeopleAmount] = useState(table.maxPeopleAmount)
+  const [bill, setBill] = useState(table.bill)
+
+  const statusNames = ["Busy", "Cleaning", "Free", "Reserved"]
+  const otherStatuses = statusNames.filter(statusName => statusName !== status)
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(updateSingleTable({status, peopleAmount, maxPeopleAmount, bill, id }))
+    navigate("/")
+  }
 
   return (
-    <h1>{table.id}</h1>
-    
+    <>
+      <h1>Table {table.id}</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Status</Form.Label>
+          <Form.Control as="select" onChange={e => setStatus(e.target.value)} >
+            <option value={status}>
+              {status}
+            </option>
+            {
+              otherStatuses.map(statusName => (
+                <option key={shortid()} value={statusName}>
+                  {statusName}
+                </option>
+              ))
+            }
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>People</Form.Label>
+          <Form.Control
+            type="text"
+            value={peopleAmount} 
+            onChange={e => setPeopleAmount(e.target.value)}  
+          />
+          <Form.Control
+            type="text"
+            value={maxPeopleAmount} 
+            onChange={e => setMaxPeopleAmount(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Bill</Form.Label>
+          <Form.Control
+            type="text"
+            value={bill} 
+            onChange={e => setBill(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">Update</Button>
+      </Form>
+    </>
   )
 }
 
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -4,10 +4,12 @@ export const getTableId = ({ tables }, tableId) => tables.find(table => table.id
 
 // action names
 const createActionName = name => `app/tables/${name}`;
-const UPDATE_TABLES = createActionName('UPDATE_TableS')
+const UPDATE_TABLES = createActionName('UPDATE_TABLESS')
+const EDIT_TABLE = createActionName('EDIT_TABLE')
 
 // action creators
 export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
+export const editTable = payload => ({ type: EDIT_TABLE, payload })
 export const fetchTables = () => {
   return (dispatch) => {
     fetch('http://localhost:3131/api/tables')
@@ -15,11 +17,34 @@ export const fetchTables = () => {
       .then(tables => dispatch(updateTables(tables)))
     };
 };
+export const updateSingleTable = tableData => {
+  return (dispatch) => {
+    const options = {
+      method: 'PATCH',
+    
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    
+      body: JSON.stringify({
+        status: tableData.status,
+        peopleAmount: tableData.peopleAmount,
+        maxPeopleAmount: tableData.maxPeopleAmount,
+        bill: tableData.bill
+      })
+    };
+    
+    fetch(`http://localhost:3131/tables/${tableData.id}`, options)
+      .then(() => dispatch(editTable(tableData)))
+  }
+}
 
 const tablesReducer = (statePart = [], action) => {
   switch(action.type) {
     case UPDATE_TABLES:
-        return [...action.payload]
+      return [...action.payload]
+    case EDIT_TABLE:
+      return statePart.map(table => table.id === action.payload.id ? { ...table, ...action.payload } : table)
     default:
       return statePart;
   }
