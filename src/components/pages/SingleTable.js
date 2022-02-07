c014bffbb61cdb8a5153a9e1aa2a6bda459e2135
Marta Nara <marta.kos1230@gmail.com>
2022-02-07 15:26:39 +0100

    Adjust form view in SingleTable.js

--- src/components/pages/SingleTable.js
+++ src/components/pages/SingleTable.js
@@ -1,3 +1,4 @@
+import styles from './SingleTable.module.scss';
 import { Form, Button } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux';
@@ -33,42 +34,64 @@ const SingleTable = () => {
       <h1>Table {table.id}</h1>
       <Form onSubmit={handleSubmit}>
         <Form.Group>
-          <Form.Label>Status</Form.Label>
-          <Form.Control as="select" onChange={e => setStatus(e.target.value)} >
-            <option value={status}>
-              {status}
-            </option>
-            {
-              otherStatuses.map(statusName => (
-                <option key={shortid()} value={statusName}>
-                  {statusName}
-                </option>
-              ))
-            }
-          </Form.Control>
+        <div className="d-flex justify-content-start align-items-center mt-3">
+          <div className={styles.label}>
+            <Form.Label>Status:</Form.Label>
+          </div>
+          <div className={styles.select}>
+            <Form.Control as="select" onChange={e => setStatus(e.target.value)} >
+              <option value={status}>
+                {status}
+              </option>
+              {
+                otherStatuses.map(statusName => (
+                  <option key={shortid()} value={statusName}>
+                    {statusName}
+                  </option>
+                ))
+              }
+            </Form.Control>
+          </div>
+        </div>
         </Form.Group>
         <Form.Group>
-          <Form.Label>People</Form.Label>
-          <Form.Control
-            type="text"
-            value={peopleAmount} 
-            onChange={e => setPeopleAmount(e.target.value)}  
-          />
-          <Form.Control
-            type="text"
-            value={maxPeopleAmount} 
-            onChange={e => setMaxPeopleAmount(e.target.value)}
-          />
+        <div className="d-flex justify-content-start align-items-center mt-3">
+          <div className={styles.label}>
+            <Form.Label>People:</Form.Label>
+          </div>
+          <div className={styles.numberInput}>
+            <Form.Control
+              type="text"
+              value={peopleAmount} 
+              onChange={e => setPeopleAmount(e.target.value)}  
+            />
+          </div>
+          <p className="mx-2 mt-3">/</p>
+          <div className={styles.numberInput}>
+            <Form.Control
+              type="text"
+              value={maxPeopleAmount} 
+              onChange={e => setMaxPeopleAmount(e.target.value)}
+            />
+          </div>
+        </div>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Bill</Form.Label>
-          <Form.Control
-            type="text"
-            value={bill} 
-            onChange={e => setBill(e.target.value)}
-          />
+        <div className="d-flex justify-content-start align-items-center mt-2">
+          <div className={styles.label}>
+            <Form.Label>Bill:</Form.Label>
+          </div>
+          <p className="mx-2 mt-3">$</p>
+          <div className={styles.numberInput}>
+            <Form.Control
+              type="text"
+              value={bill} 
+              onChange={e => setBill(e.target.value)}
+            />
+          </div>
+        </div>
         </Form.Group>
-        <Button variant="primary" type="submit">Update</Button>
+        <Button variant="primary" type="submit" className="mt-4">Update</Button>
       </Form>
     </>
   )
