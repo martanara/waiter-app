3cec9dbf2a1af8beb2efd3f03892d1d85fcc1921
Marta Nara <marta.kos1230@gmail.com>
2022-03-03 14:07:43 +0100

    Prepare project for deployment

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,3 +1,5 @@
+import { API_URL } from '../config';
+
 //selectors
 export const getAllTables = state => state.tables;
 export const getTableId = ({ tables }, tableId) => tables.find(table => table.id === tableId);
@@ -12,7 +14,7 @@ export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
 export const editTable = payload => ({ type: EDIT_TABLE, payload })
 export const fetchTables = () => {
   return (dispatch) => {
-    fetch('http://localhost:3131/api/tables')
+    fetch(`${API_URL}/tables`)
       .then(res => res.json())
       .then(tables => dispatch(updateTables(tables)))
     };
@@ -23,7 +25,7 @@ export const updateSingleTable = tableData => {
       method: 'PATCH',
     
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
     
       body: JSON.stringify({
@@ -34,7 +36,7 @@ export const updateSingleTable = tableData => {
       })
     };
     
-    fetch(`http://localhost:3131/tables/${tableData.id}`, options)
+    fetch(`${API_URL}/tables/${tableData.id}`, options)
       .then(() => dispatch(editTable(tableData)))
   }
 }
